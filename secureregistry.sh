293eb3a7d6fed7d5476c7da15ae07ae290bd30a1
learnbyseven <34051943+learnbyseven@users.noreply.github.com>
2018-02-26 12:23:05 +0530

    Update secureregistry.sh

--- secureregistry.sh
+++ secureregistry.sh
@@ -6,3 +6,19 @@ update-ca-trust
 systemctl restart docker
 docker run -d --name=registry -v /certs:/certs -e REGISTRY_HTTP_ADDR=0.0.0.0:5000 -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key -p 5001:5000 registry:2
 
+FOR DOCKER DAEMON 
+cat /etc/docker/
+certs.d/     daemon.json  key.json     
+[root@node2 certs]# cat /etc/docker/daemon.json 
+{
+  "debug": true,
+  "tls": true,
+  "tlscert": "/certs/domain.crt",
+  "tlskey": "/certs/domain.key",
+  "hosts": ["tcp://node2.192.168.124.112.nip.io:2376"]
+} 
+cat /certs/domain.crt > /root/.docker/ca.pem 
+copy ca.pem to all other hosts to access docker daemon on secure port with certs 
+test command:
+
+docker --tlsverify -H tcp://node2.192.168.124.112.nip.io:2376 images
